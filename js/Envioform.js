e2759d0c5e58e48173041d2a14bd5306110441f6
RodrigoIbanezUFV <9106387@alumnos.ufv.es>
2025-05-23 20:12:23 +0200

    Mandar emails de contacto

--- js/Envioform.js
+++ js/Envioform.js
@@ -1,19 +1,20 @@
-(function(){
-  emailjs.init("EkcS8rhYe7S7SbRbA"); // Public Key
+(function () {
+  emailjs.init("EkcS8rhYe7S7SbRbA");
 })();
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   const form = document.getElementById("contact-form");
-  
+
   if (form) {
-    form.addEventListener("submit", function(event) {
+    form.addEventListener("submit", function (event) {
       event.preventDefault();
 
       emailjs.sendForm("service_l37ru64", "template_chmv914", this)
-        .then(function() {
+        .then(function () {
           console.log("Mensaje enviado correctamente");
-          window.location.href = "mensajeEnviado.html";
-        }, function(error) {
+          alert("Mensaje enviado correctamente");
+          window.location.href = "./home.html";
+        }, function (error) {
           console.error("Fallo al enviar", error);
           alert("Hubo un error al enviar el mensaje.");
         });
